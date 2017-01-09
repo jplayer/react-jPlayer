1b4c0f1dd1b22567b7277162d3f8d0914e660107
Martin Dawson <u1356770@uniportal.com>
2017-01-09 17:31:40 +0000

    Getting vsCode debugger to work

---
+++ test/components/controls/fullScreen.spec.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import expect from 'expect';
+import { shallow } from 'enzyme';
+
+import FullScreen from '../../../src/components/controls/fullScreen';
+
+describe('shallow: <FullScreen />', () => {
+  let Component;
+  let element;
+  const functions = {
+    onClick: () => null,
+  };
+  const attributes = {
+    'data-test': 'test',
+  };
+  const spy = expect.spyOn(functions, 'onClick');
+
+  beforeEach(() => {
+    Component = shallow(
+      <FullScreen onClick={functions.onClick} {...attributes}>
+        <i className="fa fa-expand" />
+      </FullScreen>,
+    );
+    element = Component.find('a');
+  });
+
+  it('calls onClick handler on click', () => {
+    element.simulate('click');
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('custom attributes override existing', () => {
+    var p = element.prop('data-test');
+    expect(p).toBe(attributes['data-test']);
+  });
+
+  it('renders childen', () => {
+    expect(element.children()).toExist();
+  });
+});
