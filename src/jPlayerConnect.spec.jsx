56731896f51eb527795d1d7531cd3ce173e70c4d
Martin <Martino1995@blueyonder.co.uk>
2017-03-03 19:27:08 +0000

    added jPlayerConnect tests

--- src/jPlayerConnect.spec.jsx
+++ src/jPlayerConnect.spec.jsx
@@ -1,11 +1,19 @@
+import React from 'react';
 import expect from 'expect';
+import { connect } from 'react-redux';
+import { shallow } from 'enzyme';
 
-import { getJPlayers } from './util/common.spec';
+import { getJPlayers, getDefaultJPlayers } from './util/common.spec';
 import { defaultOptions, statusDefaultValues } from './util/constants';
 import { __get__ } from './jPlayerConnect';
+import * as actions from './jPlayer/_actions/actions';
 
 const mapStateToProps = __get__('mapStateToProps');
+const mapDispatchToProps = __get__('mapDispatchToProps');
+const jPlayerConnect = __get__('jPlayerConnect');
 const uid = 'jPlayer-1';
+
+const MockPlayer = () => <div />;
 
 describe('JPlayerConnect', () => {
   it('maps state with custom props', () => {
@@ -19,12 +27,47 @@ describe('JPlayerConnect', () => {
   });
 
   it('custom props with same name as state get overwritten', () => {
-    const expected = mapStateToProps(getJPlayers({ muted: false }), { uid, muted: true });
+    const expected = mapStateToProps(getJPlayers({ muted: true }), { uid, muted: false });
 
     expect(expected).toEqual({
       ...defaultOptions,
       ...statusDefaultValues,
       muted: true,
     });
+  });
+
+  it('maps other players if they exist', () => {
+    const expected = mapStateToProps(getDefaultJPlayers(3, true), { uid: 'jPlayer-2' });
+
+    expect(expected).toEqual({
+      ...defaultOptions,
+      ...statusDefaultValues,
+      jPlayers: {
+        'jPlayer-1': {
+          ...defaultOptions,
+          ...statusDefaultValues,
+        },
+        'jPlayer-3': {
+          ...defaultOptions,
+          ...statusDefaultValues,
+        },
+      },
+    });
+  });
+
+  it('maps all actions to props', () => {
+    const expected = mapDispatchToProps(Function.prototype);
+    const jPlayerActions = actions;
+    delete jPlayerActions.default;
+
+    expect(expected).toIncludeKeys(Object.keys(jPlayerActions));
+  });
+
+  it('connects player to store', () => {
+    const Component = jPlayerConnect(MockPlayer);
+    const wrapper = shallow(<Component test="test" />);
+
+    expect(wrapper.prop('uid')).toBe('MockPdlayer');
+    expect(wrapper.prop('test')).toBe('test');
   });
 });
