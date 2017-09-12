cafc054cb450e0f4ac4d3ea38605dfa97094fee1
Martin Dawson <martino1995@blueyonder.co.uk>
2017-09-12 10:06:20 +0100

    added titleContainer unit test

--- src/components/title/titleContainer.spec.jsx
+++ src/components/title/titleContainer.spec.jsx
@@ -1,26 +1,36 @@
-// import React from 'react';
-// import expect from 'expect';
-// import proxyquire from 'proxyquire';
+import React from 'react';
+import expect from 'expect';
+import proxyquire from 'proxyquire';
 
-// import containerSetup from '../../util/specHelpers/containerSetup.spec';
+import containerSetup from '../../util/specHelpers/containerSetup.spec';
 
-// proxyquire.noCallThru();
+proxyquire.noCallThru();
 
-// const id = 'TestPlayer';
-// const mockTitle = () =>
-//   <div />;
-// const titleContainer = proxyquire('./titleContainer', {
-//   './title': mockTitle,
-// }).default;
-// const setup = (jPlayers, props) => containerSetup(titleContainer, jPlayers, props);
+const id = 'TestPlayer';
+const mockTitle = () =>
+  <div />;
+const titleContainer = proxyquire('./titleContainer', {
+  './title': mockTitle,
+}).default;
+const setup = (jPlayers, props) => containerSetup(titleContainer, jPlayers, props);
 
-// describe('TitleContainer', () => {
-//   let jPlayers;
+describe('TitleContainer', () => {
+  let jPlayers;
 
-//   beforeEach(() => {
-//     jPlayers = {
-//       [id]: {
-//       },
-//     };
-//   });
-// });
+  beforeEach(() => {
+    jPlayers = {
+      [id]: {
+        media: {
+          title: 'Bubble',
+          artist: 'Kalimba',
+        },
+      },
+    };
+  });
+
+  it('passes in src', () => {
+    const { wrapper } = setup(jPlayers);
+
+    expect(wrapper.find(mockTitle).prop('title')).toBe('Kalimba - Bubble');
+  });
+});
